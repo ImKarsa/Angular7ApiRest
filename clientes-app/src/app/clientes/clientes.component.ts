13e5605c7cf8138c24bb8d48da1117c71384ca3e
Fernandez Mendez <ruben.fernandez.external@atos.net>
2019-03-27 10:15:51 +0100

    Crud funcionando, carpeta 5 acabada

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -21,4 +21,46 @@ export class ClientesComponent implements OnInit {
     );
   }
 
+  delete(cliente: Cliente ): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false,
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: '¿Esta seguro?',
+      text: `¿Seguro que desea eliminar al cliente &{cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, estoy seguro',
+      cancelButtonText: '¡No!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response =>{
+            this.clientes = this.clientes.filter(cli => cli !== cliente);
+          }
+        );
+
+        swalWithBootstrapButtons.fire(
+          '¡Cliente Eliminado!',
+          'El cliente seleccionado ha sido eliminado',
+          'success'
+        );
+      } else if (
+        // Read more about handling dismissals
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'El clliente no ha sido eliminado',
+          'error'
+        );
+      }
+    });
+  }
 }
